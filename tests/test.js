77acae5e901c211f515f9f77becff97d96635511
Ryan Smith <ryan.l.smith4@gmail.com>
2019-08-13 14:57:36 -0700

    [ADD] Tests

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,33 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line import/no-self-import
 // import MyDate from '../index';
-const MyDate = require('../index');
+const DateObj = require('../index');
+//   exports
 // import MyDate from '../index';
+
+console.log('---------------------')
+// console.log(MyDate)
+console.log('---------------------')
 
 
 test('Smoke test', () => {
-  expect(true).toBe(tssrue);
+  expect(true).toBe(true);
   expect(true).not.toBe(false);
 });
 
 test('Date Creation', () => {
-  const d = new MyDate();
+  const d = new DateObj.MyDate();
   expect(d).toHaveProperty('date');
 });
 
 test('Should only return the year', () => {
   const c = new Date();
-  const d = new MyDate();
-  expect(d.year()).toBe(c.getFullYear());
+  const d = new DateObj.MyDate();
+  expect(d.year()).toBe(String(c.getFullYear()));
 });
 
+test('Should only return the month', () => {
+  const c = String(new Date()).split(' ');
+  const d = new DateObj.MyDate();
+  expect(d.month()).toBe(String(c[1]));
+});
